20036673fffcdf2f956bb75f733fe1b25297f0d7
hugopassos <hugo.passos@hotmail.com>
2019-09-05 12:37:14 -0300

    created the function switch_player

--- bin/main.rb
+++ bin/main.rb
@@ -34,18 +34,28 @@ def get_names
   $player_two = Player.new(name, weapon)
 end
 
+def switch_player
+  if $active_player == $player_one.name
+    $active_player = $player_two.name
+    $active_weapon = $player_two.weapon
+  elsif active_player == $player_two.name
+    $active_player = $player_one.name
+    $active_weapon = $player_one.weapon
+  end
+end
+
 get_names
 
 square_values = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-active_player = $player_one.name
-active_weapon = $player_one.weapon
+$active_player = $player_one.name
+$active_weapon = $player_one.weapon
 
-puts active_player
-puts active_weapon
+puts $active_player
+puts $active_weapon
 
 draw_board(square_values)
-puts "#{active_player}, choose a square: "
+puts "#{$active_player}, choose a square: "
 choice = gets.chomp
 
 while square_values[choice[0].to_i][choice[1].to_i] != " "
@@ -53,4 +63,15 @@ while square_values[choice[0].to_i][choice[1].to_i] != " "
   choice = gets.chomp
 end
 
-square_values[choice[0].to_i][choice[1].to_i] = active_weapon
+square_values[choice[0].to_i][choice[1].to_i] = $active_weapon
+
+switch_player
+
+draw_board(square_values)
+puts "#{$active_player}, choose a square: "
+choice = gets.chomp
+
+while square_values[choice[0].to_i][choice[1].to_i] != " "
+  puts "Invalid square. Choose an empty square."
+  choice = gets.chomp
+end
