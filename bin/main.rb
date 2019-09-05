7cfb1ddcf1ae8b9a2e60cd2fbbab159a325d3781
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-09-05 19:09:30 -0400

    added game_over method

--- bin/main.rb
+++ bin/main.rb
@@ -36,7 +36,25 @@ def switch_player(active_player, player_one, player_two)
     active_player = player_one.name
     active_weapon = player_one.weapon
   end
+  puts "Im inside the method #{active_player}"
+  return active_player, active_weapon
 end
+
+def determine_winner(weapon, square_values, game_over)
+  game_over = true if square_values[0] == square_values[1] && square_values[0] == square_values[2]
+  game_over = true if square_values[3] == square_values[4] && square_values[3] == square_values[5]
+  game_over = true if square_values[6] == square_values[7] && square_values[6] == square_values[8]
+
+  game_over = true if square_values[0] == square_values[3] && square_values[0] == square_values[6]
+  game_over = true if square_values[1] == square_values[4] && square_values[1] == square_values[7]
+  game_over = true if square_values[2] == square_values[5] && square_values[2] == square_values[8]
+
+  game_over = true if square_values[0] == square_values[4] && square_values[0] == square_values[8]
+  game_over = true if square_values[2] == square_values[4] && square_values[2] == square_values[6]
+
+  game_over
+end
+
 
 player_one = get_name("one", "X")
 player_two = get_name("two", "O")
@@ -49,25 +67,39 @@ active_weapon = player_one.weapon
 puts active_player
 puts active_weapon
 
-draw_board(square_values)
-puts "#{active_player}, choose a square: "
-choice = gets.chomp.to_i
-choice -= 1
+count = 0
+game_over = false
 
-while square_values[choice.to_i] != " "
-  puts "Invalid square. Choose an empty square."
-  choice = gets.chomp
-end
+while game_over == false
+  draw_board(square_values)
 
-square_values[choice.to_i] = active_weapon
+  puts "#{active_player}, choose a square: "
+  choice = gets.chomp.to_i
+  choice -= 1
 
-switch_player(active_player, player_one, player_two)
+  while square_values[choice.to_i] != " "
+    puts "Invalid square. Choose an empty square."
+    choice = gets.chomp
+  end
 
-draw_board(square_values)
-puts "#{active_player}, choose a square: "
-choice = gets.chomp
+  square_values[choice.to_i] = active_weapon
 
-while square_values[choice.to_i] != " "
-  puts "Invalid square. Choose an empty square."
-  choice = gets.chomp
+  game_over = determine_winner(active_weapon, square_values, game_over)
+
+  # logic for draw
+  if count == 9
+    game_over = true
+    draw_board(square_values)
+    puts "Game draw."
+  end
+
+  if game_over == false
+    puts "Im outside the method #{active_player}"
+
+    active_player, active_weapon = switch_player(active_player, player_one, player_two)
+
+    puts "Im back outside the method #{active_player}"
+  end
+
+  count += 1
 end
