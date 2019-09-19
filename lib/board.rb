e469c04e82b63a1a0d6a2bd5cd2a02913c1b2594
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-09-19 13:30:24 -0400

    fixed stickler errors

--- lib/board.rb
+++ lib/board.rb
@@ -9,31 +9,31 @@ class Board
   def draw_board
     puts '  ¹     | ²     | ³     '
     puts '        |       |       '
-    puts '    #{@square_values[0]}   |   #{@square_values[1]}   |   #{@square_values[2]}   '
+    puts "    #{@square_values[0]}   |   #{@square_values[1]}   |   #{@square_values[2]}   "
     puts '        |       |       '
     puts '        |       |       '
     puts ' -----------------------'
     puts '  ⁴     | ⁵     | ⁶     '
     puts '        |       |       '
-    puts '    #{@square_values[3]}   |   #{@square_values[4]}   |   #{@square_values[5]}   '
+    puts "    #{@square_values[3]}   |   #{@square_values[4]}   |   #{@square_values[5]}   "
     puts '        |       |       '
     puts '        |       |       '
     puts ' -----------------------'
     puts '  ⁷     | ⁸     | ⁹     '
     puts '        |       |       '
-    puts '    #{@square_values[6]}   |   #{@square_values[7]}   |   #{@square_values[8]}   '
+    puts "    #{@square_values[6]}   |   #{@square_values[7]}   |   #{@square_values[8]}   "
     puts '        |       |       '
     puts '        |       |       '
   end
 
   def square_play(choice, active_weapon)
     if choice.negative? || choice > 8
-      false, 'Invalid square. Values must be between 1 and 9.'
+      return false, 'Invalid square. Values must be between 1 and 9.'
     elsif @square_values[choice] == ' '
       @square_values[choice] = active_weapon
-      true, ''
+      return true, ''
     else
-      false, 'Invalid square. Choose an empty square.'
+      return false, 'Invalid square. Choose an empty square.'
     end
   end
 end
